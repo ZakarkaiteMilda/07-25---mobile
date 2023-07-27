44f819527fd7532d39e73408bf84e7b9bdc51ce5
ZakarkaiteMilda <zakarkaite.milda@gmail.com>
2023-07-28 02:54:28 +1400

    Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import style from './Home.module.css';
+import logo from '../../assets/img/logo.jpg';
 
 export function Home() {
     return (
-        <div>
-            <img src="/" alt="/" />
-            <h1>Shopping platform</h1>
-            <link rel="stylesheet" href="/" />
-            {/* <button>Get started</button> */}
+        <div className={style.home}>
+            <img className={style.logo} src={logo} alt="Img logo" />
+            <h1>KAYAK shopping platform</h1>
+            <Link className={style.link} to='/' >Get started</Link>
         </div>
     );
 }
--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import style from './Page404.module.css';
+import Img404 from '../../assets/img/img404.png';
 
 export function Page404() {
     return (
-        <div>
-            PAGE404 CONTENT
+        <div className={style.Page404}>
+            <img className={style.Img404} src={Img404} alt="404 Img" />
+            <h1>404</h1>
+            <p>Page not found</p>
+            <Link className={style.link} to='/' >Go Home</Link>
         </div>
     );
 }
