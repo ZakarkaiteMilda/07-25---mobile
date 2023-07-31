0765310795a6812519c949fba126bba13f0df378
ZakarkaiteMilda <zakarkaite.milda@gmail.com>
2023-08-01 00:51:39 +1400

    Layout3

--- src/components/Header2/Header2.jsx
+++ src/components/Header2/Header2.jsx
@@ -3,7 +3,7 @@ import style from './Header2.module.css';
 export function Header2() {
     return (
             <header>
-                
+                <h1 className={style.h1} >Lorem ipsum dolor sit amet.</h1>
             </header>
     );
 }
--- src/components/Header3/Header3.jsx
+++ src/components/Header3/Header3.jsx
@@ -1,9 +1,12 @@
 import style from './Header3.module.css';
+import img from '../../assets/img/logo.jpg';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 export function Header3() {
     return (
             <header>
-                <img className={style.img} src="" alt="" />
+                 <img className={style.img} src={img} alt="Logo img" />
+                 <GiHamburgerMenu className={style.burger}  size='4rem' color='brown'/>
             </header>
     );
 }
