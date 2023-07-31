026f4ba726d164d81f9f8c3313f6f5eddf6b7bcf
ZakarkaiteMilda <zakarkaite.milda@gmail.com>
2023-08-01 02:52:03 +1400

    Fix page

--- src/components/CloseIcon/CloseIcon.jsx
+++ src/components/CloseIcon/CloseIcon.jsx
@@ -1,9 +1,9 @@
-import style from './CloseIxon.module.css';
+import style from './CloseIcon.module.css';
 import { GrClose } from 'react-icons/gr';
 
 export function CloseIcon() {
     return (
-            <div>
+            <div className={style.container}>
                 <GrClose className={style.close}  size='4rem' color='brown'/>
             </div>
     );
